0166bd5d3ca5825232939c170f79e113f4c49f02
Ceedrich <cedric@lehr.ch>
2024-07-04 17:14:00 +0200

    nvim: remove unnecessary keymaps

--- nvim/.config/nvim/lua/ceedrich/core/keymaps.lua
+++ nvim/.config/nvim/lua/ceedrich/core/keymaps.lua
@@ -2,12 +2,6 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc= "Clear search highlights" })
-
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
 -- Window management
 -- splits
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
@@ -20,4 +14,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
