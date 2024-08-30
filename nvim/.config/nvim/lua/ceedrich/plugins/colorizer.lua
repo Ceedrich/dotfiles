df11941e94eca50b1237884222f7d4bbf21ce1de
Ceedrich <cedric@lehr.ch>
2024-08-30 13:05:45 +0200

    nvim: update colorizer to use more modern and maintained fork

--- nvim/.config/nvim/lua/ceedrich/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/ceedrich/plugins/colorizer.lua
@@ -1,9 +1,22 @@
 return {
-	"norcalli/nvim-colorizer.lua",
+	"NvChad/nvim-colorizer.lua",
 	config = function()
-		require("colorizer").setup({ "*" }, {
-			mode = "background",
-			RRGGBBAA = true,
+		require("colorizer").setup({
+			filetypes = {
+				"*",
+				css = {
+					css = true,
+					css_fn = true,
+				},
+			},
+			user_default_options = {
+				names = false, -- "Name" codes like Blue or blue
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				mode = "background", -- Set the display mode.
+				tailwind = true, -- Enable tailwind colors
+				virtualtext = "■",
+				always_update = false,
+			},
 		})
 	end,
 }
