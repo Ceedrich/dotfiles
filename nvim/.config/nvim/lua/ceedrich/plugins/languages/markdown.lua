9cd6e9665c2f4770f446de7077788d79560f0f14
Ceedrich <cedric@lehr.ch>
2024-09-01 21:58:57 +0200

    nvim: add plugin to follow markdown links

--- nvim/.config/nvim/lua/ceedrich/plugins/languages/markdown.lua
+++ nvim/.config/nvim/lua/ceedrich/plugins/languages/markdown.lua
@@ -1,16 +1,21 @@
 return {
-	"toppair/peek.nvim",
-	event = { "VeryLazy" },
-	build = "deno task --quiet build:fast",
-	config = function()
-		local peek = require("peek")
-		peek.setup()
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			local peek = require("peek")
+			peek.setup()
 
-		vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
-		vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+			vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+			vim.api.nvim_create_user_command("PeekClose", peek.close, {})
 
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>po", peek.open, { desc = "Open markdown preview" })
-		keymap.set("n", "<leader>pc", peek.close, { desc = "Close markdown preview" })
-	end,
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>po", peek.open, { desc = "Open markdown preview" })
+			keymap.set("n", "<leader>pc", peek.close, { desc = "Close markdown preview" })
+		end,
+	},
+	{
+		"jghauser/follow-md-links.nvim",
+	},
 }
