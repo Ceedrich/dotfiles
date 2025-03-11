ece54aa8ef873e793af08cfd7edd2d6d8853aabe
Cedric Lehr <info@ceedri.ch>
2025-03-11 12:46:42 +0100

    add moodle link

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,16 @@
   ];
   home.stateVersion = "24.11";
 
+  xdg.desktopEntries = {
+    moodle = {
+      name = "Moodle";
+      exec = "xdg-open https://moodle.epfl.ch";
+      terminal = false;
+      comment = "Opens Moodle in the default browser";
+      icon = ./assets/EPFL.png;
+    };
+  };
+
   home.username = "ceedrich";
   home.homeDirectory = "/home/ceedrich";
 
