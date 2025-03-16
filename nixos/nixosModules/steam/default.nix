0dc9fb7ab87431ee66a2a2aaf145a5de90251238
Cedric Lehr <info@ceedri.ch>
2025-03-16 14:10:42 +0100

    update nixos config

--- nixos/nixosModules/steam/default.nix
+++ nixos/nixosModules/steam/default.nix
@@ -1,17 +1,22 @@
-{ pkgs, lib, ... }: {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "steam"
-    "steam-original"
-    "steam-unwrapped"
-    "steam-run"
-  ];
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
+{ pkgs, lib, config, ... }: {
+  options = {
+    steam.enable = lib.mkEnableOption "enable steam";
   };
-  programs.gamemode.enable = true;
+  config = lib.mkIf config.steam.enable {
+    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+      "steam-run"
+    ];
+    programs.steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    programs.gamemode.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    mangohud
-  ];
+    environment.systemPackages = with pkgs; [
+      mangohud
+    ];
+  };
 }
--- nixos/utils.nix
+++ nixos/utils.nix
@@ -8,4 +8,10 @@
     )
     names
   );
+  warnIfUnfree = pkg:
+    if (pkg.meta.licence.free or true == false) then
+      builtins.warn
+        "Warning: Using unfree package ${pkg.name}"
+        pkg
+    else pkg;
 }
