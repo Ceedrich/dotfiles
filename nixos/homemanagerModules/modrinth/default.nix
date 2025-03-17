bdfed9a4f6bb96e1b23a607cd8f581e761d915ab
Cedric Lehr <info@ceedri.ch>
2025-03-17 14:48:01 +0100

    update modrinth unfree

--- nixos/homemanagerModules/modrinth/default.nix
+++ nixos/homemanagerModules/modrinth/default.nix
@@ -5,6 +5,10 @@
     modrinth.enable = lib.mkEnableOption "enable modrinth";
   };
   config = lib.mkIf config.modrinth.enable {
+    allowedUnfree = [
+      "modrinth-app"
+      "modrinth-app-unwrapped"
+    ];
     home.packages = with pkgs; [ modrinth-app ];
   };
 }
