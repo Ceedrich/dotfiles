b2c484c9cc4474b77eb0aaaefe013dc629505ac4
Cedric Lehr <info@ceedri.ch>
2025-03-19 10:07:26 +0100

    add nix snippets

--- nixos/homemanagerModules/neovim/plugin/luasnip.lua
+++ nixos/homemanagerModules/neovim/plugin/luasnip.lua
@@ -2,26 +2,46 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
-
-local function clone(args, parent, user_args)
-  return args[1][1]
-end
+local extras = require("luasnip.extras")
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
 
 return {
   ls.add_snippets("nix", {
-    s("mod", {
-      t({ "{ lib, config, ... }:", "", "{", "" }),
-      t({ "  options = {", "    " }),
-      i(1),
-      t('.enable = lib.mkEnableOption "enable '),
-      f(clone, { 1 }),
-      t({ '";', "  };", "" }),
-      t("  config = lib.mkIf config."),
-      f(clone, { 1 }),
-      t({ ".enable {", "    " }),
-      i(2),
-      t({ "", "  };", "}" }),
-    }),
+    s(
+      "mod",
+      fmt(
+        [[
+      {{ lib, config, ... }}:
+
+      {{
+        options = {{
+          {}.enable = lib.mkEnableOption "enable {}";
+        }};
+        config = lib.mkIf config.{}.enable {{
+
+        }};
+      }}
+      ]],
+        {
+          i(1),
+          rep(1),
+          rep(1),
+        }
+      )
+    ),
+    s(
+      "shell",
+      fmt(
+        [[
+    {{ pkgs ? import <nixpkgs> {{}} }}:
+
+    pkgs.mkShell {{
+
+    }}
+    ]],
+        {}
+      )
+    ),
   }),
 }
