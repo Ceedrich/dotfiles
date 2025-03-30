6f67c33b553420c558873712a12345f7cdf9e511
Cedric Lehr <info@ceedri.ch>
2025-03-30 20:10:01 +0200

    update nixos config

--- nixos/hosts/fun-machine/configuration.nix
+++ nixos/hosts/fun-machine/configuration.nix
@@ -7,16 +7,61 @@
 
   services.tailscale.enable = true;
 
+  services.udev.enable = true;
+  boot.kernelModules = [ "sg" ];
+
+  users.users.arm = {
+    isNormalUser = true;
+    description = "arm";
+    group = "arm";
+    extraGroups = [ "arm" "cdrom" "video" "render" "docker" ];
+  };
+
+  users.groups.arm = { };
+
+  virtualisation = {
+    docker.enable = true;
+    oci-containers.backend = "docker";
+  };
+
+  virtualisation.oci-containers.containers."arm-rippers" = {
+    autoStart = true;
+    image = "automaticrippingmachine/automatic-ripping-machine:latest";
+    ports = [ "8080:8080" ];
+
+    environment = {
+      ARM_UID = "1001";
+      ARM_GID = "985";
+    };
+
+    volumes = [
+      "/home/arm:/home/arm"
+      "/home/arm/music:/home/arm/music"
+      "/home/arm/logs:/home/arm/logs"
+      "/home/arm/media:/home/arm/media"
+      "/home/arm/config:/etc/arm/config"
+    ];
+    extraOptions = [
+      "--device=/dev/sr0:/dev/sr0"
+      "--privileged"
+    ];
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
   services.openssh = {
     enable = true;
     ports = [ 22 ];
     settings = {
-      PasswordAuthentication = true;
+      PasswordAuthentication = false;
       AllowUsers = null;
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 8080 ];
 
   environment.systemPackages = with pkgs; [
     vim
--- nixos/nixosModules/users/default.nix
+++ nixos/nixosModules/users/default.nix
@@ -22,7 +22,7 @@
 
       # Define a user account. Don't forget to set a password with ‘passwd’.
       users.users.ceedrich = mkIf cfg.ceedrich {
-        authorizedKeys = [
+        openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZd2wl9brtYcLTQCTnV5HgBOZpT6ipwh5o46HBECSqYGphgsTWLk+l4LBn8Wgol+4FxtjVcxVGP0H74g1Yw6QxnLdUb3MNd3sAR0W0NDF0jbMsZuvTqmllqiuSoZO77TpcegN5+NVCgbzma0ITP6k+xlXPnw7jTxtOLFu01neZPPfOFmZMSJvKYrEozm89AoJWN48vO77OTfRnGpblp6yrYmlp/DQ+XOPXnNW6w0QqZVH71WH5d4UhftYbCJpbymgbTHjeamjb4Ohb7wBjVcxizQsse2oHwMvOxApdGAQJ06mQ7V6txUJVfeMhK4eq3D2rrSPllbn5vt9dHAZp2V6USRPlxQQDePx04MnURREpQGIXR0FAK2XUfg9uAnYxHj30rrd9HCUELqIkhhTJYMtwnordmeisHvHVZ+QvR6OXyaIC4E9gpcRFFPWM87OCHEmzuAnei1Okdk2V8d8F4pFsx2wSSyvZG22PfufZJ+Rffj+gzOb59yHR4m+F7Cbf7Xc= ceedrich@ceedrich-ubuntu"
         ];
         isNormalUser = true;
