4a17aaba70da1b59b67c049fb8af8ec6f28c2340
Cedric Lehr <info@ceedri.ch>
2025-04-07 11:43:20 +0200

    update kanata config

--- nixos/nixosModules/kanata/default.nix
+++ nixos/nixosModules/kanata/default.nix
@@ -1,55 +1,57 @@
-{ pkgs, ... }:
-let 
-  notify-send = "${pkgs.libnotify}/bin/notify-send";
+{ pkgs, lib, ... }:
+let
+  notify-send = lib.getExe pkgs.libnotify;
 in
 {
   hardware.uinput.enable = true;
-  services.kanata = {
-    enable = true;
+  environment.systemPackages = with pkgs; [ libnotify ];
+  services.kanata =
+    {
+      enable = true;
 
-    package = pkgs.kanata-with-cmd;
+      package = pkgs.kanata-with-cmd;
 
-    keyboards.default = {
-      extraDefCfg = ''
-        danger-enable-cmd yes 
-        process-unmapped-keys yes
-      '';
-      config = ''
-        (defsrc
-          caps esc
-          a s d f j k l ;
-          lmet lalt lsft lctl rctl rsft ralt rmet
-        )
-        (defvar
-          tap-time 200
-          hold-time 150
-        )
+      keyboards.default = {
+        extraDefCfg = ''
+          danger-enable-cmd yes 
+          process-unmapped-keys yes
+        '';
+        config = ''
+          (defsrc
+            caps esc
+            a s d f j k l ;
+            lmet lalt lsft lctl rctl rsft ralt rmet
+          )
+          (defvar
+            tap-time 200
+            hold-time 150
+          )
 
-        (defalias
-          a (tap-hold $tap-time $hold-time a lmet)
-          s (tap-hold $tap-time $hold-time s lalt)
-          d (tap-hold $tap-time $hold-time d lsft)
-          f (tap-hold $tap-time $hold-time f lctl)
-          j (tap-hold $tap-time $hold-time j rctl)
-          k (tap-hold $tap-time $hold-time k rsft)
-          l (tap-hold $tap-time $hold-time l ralt)
-          ; (tap-hold $tap-time $hold-time ; rmet)
-          switch-to-none (layer-switch none)
-          switch-to-base (layer-switch base)
-        )
+          (defalias
+            a (tap-hold $tap-time $hold-time a lmet)
+            s (tap-hold $tap-time $hold-time s lalt)
+            d (tap-hold $tap-time $hold-time d lsft)
+            f (tap-hold $tap-time $hold-time f lctl)
+            j (tap-hold $tap-time $hold-time j rctl)
+            k (tap-hold $tap-time $hold-time k rsft)
+            l (tap-hold $tap-time $hold-time l ralt)
+            ; (tap-hold $tap-time $hold-time ; rmet)
+            switch-to-none (layer-switch none)
+            switch-to-base (layer-switch base)
+          )
 
-        (deflayer base
-          esc (multi @switch-to-none (cmd ${notify-send} "Keyboard" "Using default keybinds"))
-          @a @s @d @f @j @k @l @;
-          XX XX XX XX XX XX XX XX  
-        )
+          (deflayer base
+            esc (multi @switch-to-none (cmd sh -e ${notify-send} "Keyboard" "Using default keybinds"))
+            @a @s @d @f @j @k @l @;
+            XX XX XX XX XX XX XX XX  
+          )
 
-        (deflayer none
-          esc (multi @switch-to-base (cmd ${notify-send} "Keyboard" "Using home-row-mods"))
-          a s d f j k l ;
-          lmet lalt lsft lctl rctl rsft ralt rmet
-        )
-      '';
+          (deflayer none
+            esc (multi @switch-to-base (cmd ${notify-send} "Keyboard" "'Using home-row-mods'"))
+            a s d f j k l ;
+            lmet lalt lsft lctl rctl rsft ralt rmet
+          )
+        '';
+      };
     };
-  };
 }
