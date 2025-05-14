c5ddb411a0c2ccfda077bd3dbfe5dbc4e4a9f64c
Cedric Lehr <info@ceedri.ch>
2025-05-14 18:05:38 +0200

    fix power-menu script to work on ubuntu

--- nixos/users/ubuntu.nix
+++ nixos/users/ubuntu.nix
@@ -9,7 +9,15 @@
   rofi.enable = true;
 
   home.packages = [
-    (import ../homemanagerModules/hyprland/rofi/power-menu.nix { inherit pkgs; })
+    (import ../homemanagerModules/hyprland/rofi/power-menu.nix {
+      inherit pkgs;
+
+      lockCommand = "/bin/loginctl lock-session self";
+      logoutCommand = "/bin/loginctl kill-session self";
+      shutdownCommand = "/bin/systemctl poweroff";
+      rebootCommand = "/bin/systemctl reboot";
+      suspendCommand = "/bin/systemctl suspend";
+    })
   ];
 
 
