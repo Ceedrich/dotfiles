46739551bfbbe2451c43adc76765086f5138d076
Cedric Lehr <info@ceedri.ch>
2025-05-30 13:36:55 +0200

    51 current  2025-05-30 13:36:37  25.05.20250525.7c43f08     6.12.30                          *
    update rebuild-system script

--- nixos/packages/rebuild_system.nix
+++ nixos/packages/rebuild_system.nix
@@ -4,13 +4,13 @@ let
     set -e
     pushd ~/dotfiles/nixos/
     git diff -U0 .
-    echo "Nixos Rebuilding"
+    echo "Nixos Rebuilding..."
     sudo nixos-rebuild switch --flake . &>nixos-switch.log || (
       echo hello &&
       cat nixos-switch.log | grep --color error && false
     )
     gen=$(nixos-rebuild list-generations | grep current)
-    git commit -am "$gen"
+    git commit -am "$(hostname): $gen"
     popd
   '';
 in
