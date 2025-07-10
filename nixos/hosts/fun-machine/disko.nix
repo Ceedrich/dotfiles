813338ddbfba35dd2c8bc4e78b8e85d9249720b3
Cedric Lehr <info@ceedri.ch>
2025-07-10 17:41:45 +0200

    update fun-machine config

--- nixos/hosts/fun-machine/disko.nix
+++ nixos/hosts/fun-machine/disko.nix
@@ -9,17 +9,20 @@
           partitions = {
             boot = {
               name = "boot";
-              size = "1M";
-              type = "EF02";
-            };
-            esp = {
-              name = "ESP";
-              size = "512M";
+              size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            swap = {
+              name = "swap";
+              size = "4G";
+              content = {
+                type = "swap";
               };
             };
             root = {
