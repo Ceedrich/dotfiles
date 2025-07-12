6729dc5e90dee557e30fc4e06881f84d4505169d
Cedric Lehr <info@ceedri.ch>
2025-07-12 16:04:22 +0200

    remove obsolete neovim config

--- nixos/homemanagerModules/neovim/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {},
-  auto_install = false,
-  highlight = { enable = true, disable = "latex" },
-  ident = { enable = true },
-  autotag = { enable = true },
-})
