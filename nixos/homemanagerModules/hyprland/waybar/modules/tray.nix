6edfced37f4a457fe9a8eab5c25cfef7682005c1
Cedric Lehr <info@ceedri.ch>
2025-11-10 23:06:00 +0100

    gaming: 237 current  2025-11-10 23:05:52  25.05.20251107.6faeb06     6.12.57                          *

--- nixos/homemanagerModules/hyprland/waybar/modules/tray.nix
+++ nixos/homemanagerModules/hyprland/waybar/modules/tray.nix
@@ -3,12 +3,14 @@
   settings = {
     ${name} = {
       icon-size = 21;
-      spacing = 10;
+      spacing = 8;
     };
   };
   style =
     #css
     ''
-
+      #tray {
+          padding: 8px;
+      }
     '';
 }
