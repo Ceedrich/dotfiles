3e1d8f44453b9f6e3fa6c2c7c35f263e1c3f6ec3
Cedric Lehr <info@ceedri.ch>
2025-11-10 21:10:02 +0100

    gaming: 196 current  2025-11-10 21:01:04  25.05.20251107.6faeb06     6.12.57                          *

---
+++ nixos/homemanagerModules/hyprland/waybar/modules/clock.nix
@@ -0,0 +1,27 @@
+{}: rec {
+  name = {};
+  settings.${name} = {
+    format = "{=%H=%M}  ";
+    tooltip-format = "{=%A; %B %d; %Y (%R)}<br><tt><small>{calendar}</small></tt>";
+    calendar = {
+      mode = "month";
+      weeks-pos = "left";
+      on-scroll = 1;
+      format = {
+        months = "<b>{}</b>";
+        days = "<b>{}</b>";
+        weeks = "<b>{}</b>";
+        weekdays = "<b>{}</b>";
+        today = "<b><u>{}</u></b>";
+      };
+    };
+    actions = {
+      on-click = "shift_reset";
+      on-scroll-up = "shift_up";
+      on-scroll-down = "shift_down";
+    };
+  };
+  style =
+    #css
+    '''';
+}
