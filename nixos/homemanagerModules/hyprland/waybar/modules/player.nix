d1dabdfe4a40e07f43cd69e9b4245d94901c04ea
Cedric Lehr <info@ceedri.ch>
2025-11-10 19:43:51 +0100

    gaming: 179 current  2025-11-10 19:36:40  25.05.20251107.6faeb06     6.12.57                          *

--- nixos/homemanagerModules/hyprland/waybar/modules/player.nix
+++ nixos/homemanagerModules/hyprland/waybar/modules/player.nix
@@ -8,35 +8,35 @@
   next = "custom/player-next";
   main = "custom/player-main";
 in {
-  ${name} = {
-    orientation = "inherit";
-    modules = [prev main next];
-  };
-  ${main} = {
-    interval = 5;
-    tooltip = false;
-    escape = true;
-    format = "  {}";
-    exec = "${playerctl} metadata --format='{{ artist }} - {{ title }}'";
-    on-click = "${playerctl} play-pause";
-    max-length = 50;
-  };
-  ${prev} = {
-    interval = 5;
-    tooltip = false;
-    escape = true;
-    format = "{icon}";
-    format-icons = "󰒮";
-    on-click = "${playerctl} previous";
-    max-length = 50;
-  };
-  ${next} = {
-    interval = 5;
-    tooltip = false;
-    escape = true;
-    format = "{icon}";
-    format-icons = "󰒭";
-    on-click = "${playerctl} next";
-    max-length = 50;
-  };
+    ${name} = {
+      orientation = "inherit";
+      modules = [prev main next];
+    };
+    ${main} = {
+      interval = 5;
+      tooltip = false;
+      escape = true;
+      format = "  {}";
+      exec = "${playerctl} metadata --format='{{ artist }} - {{ title }}'";
+      on-click = "${playerctl} play-pause";
+      max-length = 50;
+    };
+    ${prev} = {
+      interval = 5;
+      tooltip = false;
+      escape = true;
+      format = "{icon}";
+      format-icons = "󰒮";
+      on-click = "${playerctl} previous";
+      max-length = 50;
+    };
+    ${next} = {
+      interval = 5;
+      tooltip = false;
+      escape = true;
+      format = "{icon}";
+      format-icons = "󰒭";
+      on-click = "${playerctl} next";
+      max-length = 50;
+    };
 }
