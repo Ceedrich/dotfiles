8c6ad63881d6a96dfa5b3df9fb57c73b822e4d87
Cedric Lehr <info@ceedri.ch>
2025-11-10 22:46:28 +0100

    gaming: 232 current  2025-11-10 22:46:20  25.05.20251107.6faeb06     6.12.57                          *

--- nixos/homemanagerModules/hyprland/waybar/modules/workspaces.nix
+++ nixos/homemanagerModules/hyprland/waybar/modules/workspaces.nix
@@ -8,13 +8,6 @@
         color: @overlay0;
         padding: 0px 4px;
         margin: 2px 4px;
-        border-bottom: 2px solid;
-      }
-
-      #workspaces button.focused {
-        background-color: rgba(0, 0, 0, 0.3);
-        color: @rosewater;
-        border-color: @rosewater;
       }
 
       #workspaces button.active {
