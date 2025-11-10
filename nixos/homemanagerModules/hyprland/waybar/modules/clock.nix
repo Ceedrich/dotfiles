1d22234ac797c9a4a75cefce0c5707f334336567
Cedric Lehr <info@ceedri.ch>
2025-11-10 21:26:48 +0100

    gaming: 205 current  2025-11-10 21:26:41  25.05.20251107.6faeb06     6.12.57                          *

--- nixos/homemanagerModules/hyprland/waybar/modules/clock.nix
+++ nixos/homemanagerModules/hyprland/waybar/modules/clock.nix
@@ -3,7 +3,7 @@
   settings.${name} = {
     format = "{:%H:%M}";
     interval = 1;
-    tooltip-format = "<tt><small>{:%a, %d/%m/%y %H:%M:%S}\n{calendar}</small></tt>";
+    tooltip-format = "<tt><small>{calendar}</small></tt>";
     calendar = {
       mode = "month";
       weeks-pos = "left";
