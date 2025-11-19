0d90cbea45b6fb3b2fce44692e6eb272a1b6df62
Cedric Lehr <info@ceedri.ch>
2025-11-19 13:12:35 +0100

    update passmenu script

--- nixos/packages/passmenu/default.nix
+++ nixos/packages/passmenu/default.nix
@@ -1,12 +1,22 @@
 {
+  waylandSupport ? true,
+  xserverSupport ? true,
   rofi-wayland,
   pkgs,
   wtype,
   gnused,
+  xdotool,
+  lib,
 }: let
 in
   pkgs.writeShellApplication {
     name = "passmenu";
-    runtimeInputs = [rofi-wayland wtype gnused];
+    runtimeInputs =
+      [
+        rofi-wayland
+        gnused
+      ]
+      ++ lib.optional waylandSupport wtype
+      ++ lib.optional xserverSupport xdotool;
     text = builtins.readFile ./passmenu;
   }
