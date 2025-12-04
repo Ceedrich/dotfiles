54b2156d6cb3e6d02bc0ac70ccd6616f64ce574b
Cedric Lehr <info@ceedri.ch>
2025-12-04 19:18:13 +0100

    ahsoka: 307	2025-12-04	19:18:08	25.11.20251202.1aab892	6.12.60	Unknown	[]

--- nixos/hosts/jarjar/minecraft-servers/vanillaish/default.nix
+++ nixos/hosts/jarjar/minecraft-servers/vanillaish/default.nix
@@ -1,10 +1,11 @@
 {
+  lib,
   pkgs,
   ceedrichLib,
   ...
 }: let
   loader = "fabric";
-  version = "1.21.4";
+  version = "1.21.5";
   mods = [
     "rei"
     "appleskin"
@@ -36,7 +37,10 @@ in {
   ];
   services.minecraft-servers.servers."vanillaish" = {
     enable = true;
-    package = pkgs.fabricServers.fabric-1_21_4;
+    package = let
+      escapeVersion = v: lib.replaceStrings ["."] ["_"] v;
+    in
+      pkgs."${loader}Servers"."${loader}-${escapeVersion version}";
 
     whitelist = {
       "Ceedrich" = "1764144b-fec5-4dbd-bd93-2b9e2530fa05";
@@ -56,16 +60,30 @@ in {
       "mods" = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues (
         import ./mods.nix {inherit (pkgs) fetchurl;}
       ));
+      "server-icon.png" = ./server-icon.png;
     };
     files = {
       "config/melius-commands/commands/info.json".value = {
         "id" = "info";
         "executes" = [
           {
-            "command" = ''tellraw @s "hello there"'';
+            "command" = "tellraw @s [\"\",{\"text\":\"Ceedrich's Minecraft Server\",\"bold\":true,\"underlined\":true,\"color\":\"dark_green\"},\"\\n\",\"\\n\",{\"text\":\"Seed: \",\"color\":\"green\"},{\"text\":\"6942694997652268\",\"underlined\":true,\"color\":\"green\",\"clickEvent\":{\"action\":\"copy_to_clipboard\",\"value\":\"6942694997652268\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Click to copy to clipboard\",\"italic\":true}]}},\"\\n\",\"\\n\",{\"text\":\"Commands:\",\"bold\":true,\"color\":\"gold\"},\"\\n\",{\"text\":\"\/info\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/info\"}},\" shows this information panel about the server\",\"\\n\",{\"text\":\"\/coords\",\"italic\":true,\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/coords\"}},\" lets you see\/manage the coordinates of different locations on the server. If you built  a farm or found a nice biome, let the others know with the command \",{\"text\":\"\/coords setplace <name>\",\"underlined\":true,\"color\":\"dark_aqua\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/coords setplace \"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Share your coordinates\",\"italic\":true}]}},\"\\n\",{\"text\":\"\/autojoingroup\",\"italic\":true,\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/autojoingroup\"}},\" lets you automatically join a voice chat group when you join the server. Use \",{\"text\":\"\/autojoingroup set <name>\",\"underlined\":true,\"color\":\"dark_aqua\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/autojoingroup set \"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Set an auto join group\",\"italic\":true}]}},\" to set such a auto join group or \",{\"text\":\"\/autojoingroup remove <name>\",\"underlined\":true,\"color\":\"dark_aqua\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/autojoingroup remove \"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Remove an auto join group\",\"italic\":true}]}},\" to remove such a auto join group.\",\"\\n\",{\"text\":\"\/ping\",\"italic\":true,\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"\/ping\"}},\" lets you check your current ping\",\"\\n\",\"\\n\",{\"text\":\"Extra Features: \",\"bold\":true,\"color\":\"gold\"},\"\\n\",\"- Leaves decay faster\",\"\\n\",\"- Players and mobs have a chance to drop their head on death.\",\"\\n\",\"- Endermen don't pick up \/ place down blocks.\",\"\\n\",\"- You can craft small blocks by putting a block into a grindstone.\",\"\\n\",\"\\n\",{\"text\":\"List of Ideas: \",\"bold\":true,\"color\":\"gold\"},{\"text\":\"github.com\/Ceedrich\/MC-Ideas\",\"underlined\":true,\"color\":\"dark_aqua\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https:\/\/github.com\/Ceedrich\/MC-Ideas\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Open link\",\"italic\":true}]}}]";
             "op_level" = 0;
           }
         ];
+      };
+      "config/beaconExtender.json5" = {
+        format = pkgs.formats.json {};
+        value = {
+          showBeaconLayers = true;
+          maxLayers = 10;
+          rangeFunctionType = "LINEAR";
+          rangeFunctionParam1 = 10.0;
+          rangeFunctionParam2 = 10.0;
+          effectDurationFunctionType = "LINEAR";
+          effectDurationFunctionParam1 = 10.0;
+          effectDurationFunctionParam2 = 10.0;
+        };
       };
     };
   };
