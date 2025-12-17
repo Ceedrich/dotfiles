3e043fea36e44d0ce9d8bfccf269d49e4e81d20f
Cedric Lehr <info@ceedri.ch>
2025-12-17 17:49:20 +0100

    satine: 29	2025-12-17	17:46:44	25.11.20251204.c97c47f	6.12.60	Unknown	[]

--- nixos/nixosModules/zathura/default.nix
+++ nixos/nixosModules/zathura/default.nix
@@ -10,8 +10,9 @@ in {
   };
   config = lib.mkIf cfg.enable {
     xdg.mime.defaultApplications."application/pdf" = ["org.pwmt.zathura-pdf-mupdf.desktop"];
+
     global-hm.config.programs.zathura = {
-      enable = lib.mkDefault true;
+      enable = true;
     };
   };
 }
