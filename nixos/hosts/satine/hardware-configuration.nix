318ceb54ca99ea720e9ac5c2cc688f9a7d9674a7
Cedric Lehr <info@ceedri.ch>
2025-12-29 18:30:40 +0100

    satine: 58	2025-12-29	18:21:36	25.11.20251215.c6f52eb	6.12.62	Unknown	[]

--- nixos/hosts/satine/hardware-configuration.nix
+++ nixos/hosts/satine/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.loader.grub.configurationLimit = 10;
+  boot.loader.grub.configurationLimit = 5;
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
