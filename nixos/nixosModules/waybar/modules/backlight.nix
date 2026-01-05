d0e9cc3441f2f0ccfdd7e8d86020afa6d36df427
Cedric Lehr <info@ceedri.ch>
2026-01-05 17:47:04 +0100

    satine: 64	2026-01-05	17:47:00	25.11.20251215.c6f52eb	6.12.62	Unknown	[]

--- nixos/nixosModules/waybar/modules/backlight.nix
+++ nixos/nixosModules/waybar/modules/backlight.nix
@@ -24,6 +24,7 @@ in {
         format = "{percent}% {icon}";
         format-icons = ["󱩎" "󱩎" "󱩏" "󱩐" "󱩑" "󱩒" "󱩓" "󱩔" "󱩕" "󱩖" "󰛨"];
         tooltip = false;
+        reverse-scrolling = true;
       };
     });
 
