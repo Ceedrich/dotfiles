d2f9435882b42235500e0bebe6286f6e15b36647
Cedric Lehr <info@ceedri.ch>
2026-01-07 16:40:07 +0100

    satine: 69	2026-01-07	16:35:51	25.11.20251215.c6f52eb	6.12.62	Unknown	[]

--- nixos/hosts/_graphical/default.nix
+++ nixos/hosts/_graphical/default.nix
@@ -8,9 +8,13 @@
 in {
   global-hm.config = {
     programs.brave.enable = true;
-    xdg.mimeApps.defaultApplicationPackages = [
-      pkgs.brave
-    ];
+    xdg.mimeApps.defaultApplications = {
+      "text/html" = "brave-browser.desktop";
+      "x-scheme-handler/http" = "brave-browser.desktop";
+      "x-scheme-handler/https" = "brave-browser.desktop";
+      "x-scheme-handler/about" = "brave-browser.desktop";
+      "x-scheme-handler/unknown" = "brave-browser.desktop";
+    };
     vpn.epfl = true;
   };
 
