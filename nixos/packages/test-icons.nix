848677626d954281a864256b9a69744056e74ca1
Cedric Lehr <info@ceedri.ch>
2026-01-08 11:55:44 +0100

    satine: 72	2026-01-08	11:55:41	25.11.20251215.c6f52eb	6.12.62	Unknown	[]

--- nixos/packages/test-icons.nix
+++ nixos/packages/test-icons.nix
@@ -1,16 +1,66 @@
 {
+  wayland_support ? true,
+  x11_support ? true,
+  lib,
   find-icons,
   libnotify,
+  wl-clipboard,
   writeShellApplication,
+  xclip,
 }: let
   test-icons = writeShellApplication {
     name = "test-icons";
-    runtimeInputs = [find-icons libnotify];
+    runtimeInputs =
+      [find-icons libnotify]
+      ++ lib.optional wayland_support wl-clipboard
+      ++ lib.optional x11_support xclip;
     text = ''
+      CLIPBOARD=""
+
+      usage() {
+        echo "Usage: $0 [options]"
+        echo ""
+        echo "Display a notification with an icon and copy the icon name to stdoud or clipboard"
+        echo ""
+        echo "Options: "
+        echo "-h    Show this help message and exit"
+        echo "-c    Copy icon name to the clipboard (requires wl-copy or xclip)"
+      }
+
+      fatal () {
+        code="$1"; shift
+        echo "$@" 1>&2
+        exit "$code"
+      }
+
+      copy() {
+        local text="$1"
+        if [[ -n $WAYLAND_DISPLAY ]] || [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+          command -v wl-copy >/dev/null 2>&1 || fatal 2 "'wl-copy' not found"
+          wl-copy "$text"
+        else
+          command -v xclip >/dev/null 2>&1 || fatal 2 "'xclip' not found"
+          echo -n "$text" | xclip -selection clipboard
+        fi
+      }
+
+      while getopts "hc" opt; do
+        case "$opt" in
+          h) usage; exit ;;
+          c) CLIPBOARD="yes" ;;
+          *) usage; exit 1
+        esac
+      done
+
       icon="$(find-icons)"
       [[ -n "$icon" ]] || exit 1
       notify-send "test-icon" --icon="$icon"
-      echo "$icon"
+
+      if [[ -n $CLIPBOARD ]]; then
+        copy "$icon"
+      else
+        echo "$icon"
+      fi
     '';
   };
 in
