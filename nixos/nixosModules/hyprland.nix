e16920bf37f6e93b89e9aa821f5fb7bf39612ac4
Cedric Lehr <info@ceedri.ch>
2026-01-09 17:21:19 +0100

    ahsoka: 381	2026-01-09	17:21:16	25.11.20260107.d351d06	6.12.63	Unknown	[]

--- nixos/nixosModules/hyprland.nix
+++ nixos/nixosModules/hyprland.nix
@@ -65,7 +65,7 @@ in {
 
     global-hm.config.wayland.windowManager.hyprland = {
       enable = true;
-      plugins = with pkgs.hyprlandPlugins; [hyprbars hyprspace];
+      plugins = with pkgs.hyprlandPlugins; [hyprbars hyprexpo];
 
       settings = let
         inherit
@@ -89,6 +89,10 @@ in {
           hyprbars-button = [
             "$red, 10,, hyprctl dispatch killactive"
           ];
+        };
+        plugin.hyprexpo = {
+          bg_col = "$crust";
+          skip_empty = true;
         };
 
         exec-once = autostart;
