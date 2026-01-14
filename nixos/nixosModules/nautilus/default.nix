6edce28d147708cb1c0f0118e3cdda6e1395f7f0
Cedric Lehr <info@ceedri.ch>
2026-01-14 11:51:07 +0100

    ahsoka: 388	2026-01-14	11:51:03	25.11.20260107.d351d06	6.12.63	Unknown	[]

--- nixos/nixosModules/nautilus/default.nix
+++ nixos/nixosModules/nautilus/default.nix
@@ -9,6 +9,7 @@ in {
   options.programs.nautilus = {
     enable = lib.mkEnableOption "Nautilus";
     package = lib.mkPackageOption pkgs "nautilus" {};
+    portalPackage = lib.mkPackageOption pkgs "xdg-desktop-portal-gnome" {};
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [cfg.package];
@@ -16,5 +17,6 @@ in {
     services.gvfs.enable = true;
     programs.nautilus-open-any-terminal.enable = true;
     services.gnome.sushi.enable = true;
+    xdg.portal.extraPortals = [cfg.portalPackage];
   };
 }
