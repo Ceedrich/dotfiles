ff9aded7c2b6c63b314add5b7355dc992dde4d2c
Cedric Lehr <info@ceedri.ch>
2026-01-22 17:11:16 +0100

    satine: 113	2026-01-22	17:11:13	25.11.20260117.72ac591	6.12.65	Unknown	[]

--- nixos/homemanagerModules/nwg-drawer/default.nix
+++ nixos/homemanagerModules/nwg-drawer/default.nix
@@ -10,58 +10,49 @@ in {
     enable = lib.mkEnableOption "nwg-drawer";
     package = lib.mkPackageOption pkgs "nwg-drawer" {};
   };
-  config = let
-    catppuccin-palette = pkgs.fetchurl {
-      url = "https://cdn.jsdelivr.net/npm/@catppuccin/palette/css/catppuccin.css";
-      hash = "sha256-wdDGTQ/XkfI5efbqfdBpTXgr6oX1AkjQTkVk9511IrE=";
-    };
-  in
-    lib.mkIf cfg.enable {
-      home.packages = [cfg.package];
-      xdg.configFile."nwg-drawer/drawer.css".text =
-        # css
-        builtins.readFile catppuccin-palette
-        + ''
-          window {
-              background-color: rgba(var(--ctp-mocha-base-rgb) / 0.95);
-              color: var(--ctp-mocha-text);
-          }
+  config = lib.mkIf cfg.enable {
+    home.packages = [cfg.package];
+    xdg.configFile."nwg-drawer/drawer.css".text = ''
+      window {
+          background-color: rgba(30, 30, 46, 0.95);
+          color: rgb(205, 214, 244);
+      }
 
-          /* search entry */
-          entry {
-              background-color: rgba(var(--ctp-mocha-crust-rgb) / 0.2);
-          }
+      /* search entry */
+      entry {
+          background-color: rgba(17, 17, 27, 0.2);
+      }
 
-          button, image {
-              background: none;
-              border: none
-          }
+      button, image {
+          background: none;
+          border: none
+      }
 
-          button:hover {
-              background-color: rgba(var(--ctp-mocha-text-rgb), 0.1)
-          }
+      button:hover {
+          background-color: rgba(30, 30, 46, 0.1)
+      }
 
-          /* in case you wanted to give category buttons a different look */
-          #category-button {
-              margin: 0 10px 0 10px
-          }
+      /* in case you wanted to give category buttons a different look */
+      #category-button {
+          margin: 0 10px 0 10px
+      }
 
-          #pinned-box {
-              padding-bottom: 5px;
-              border-bottom: 1px dotted gray
-          }
+      #pinned-box {
+          padding-bottom: 5px;
+          border-bottom: 1px dotted gray
+      }
 
-          #files-box {
-              padding: 5px;
-              border: 1px dotted gray;
-              border-radius: 15px
-          }
+      #files-box {
+          padding: 5px;
+          border: 1px dotted gray;
+          border-radius: 15px
+      }
 
-          /* math operation result label */
-          #math-label {
-              font-weight: bold;
-              font-size: 16px
-          }
-        '';
-    };
+      /* math operation result label */
+      #math-label {
+          font-weight: bold;
+          font-size: 16px
+      }
+    '';
+  };
 }
