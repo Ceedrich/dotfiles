f1f1c5e6ac82103c594772f06d498d10ec30fd6a
Cedric Lehr <info@ceedri.ch>
2026-02-01 23:04:26 +0100

    satine: 160	2026-02-01	23:04:23	25.11.20260117.72ac591	6.12.65	Unknown	[]

--- nixos/nixosModules/hyprland/default.nix
+++ nixos/nixosModules/hyprland/default.nix
@@ -205,7 +205,7 @@ in {
           # ++ (builtins.map (regex: "float on, match:class ${regex}") floating);
           # Up until 0.52
           ++ [
-            "float, center, title:^(Open File|Open|Save|Save As|Export|Import|Choose File|Rename), class:^(.*)$"
+            "float, center, title:^(Open File|Open|Save|Save File|Save As|Export|Import|Choose File|Rename), class:^(.*)$"
             "float, center, class:(xdg-desktop-portal-[a-zA-z0-9]*)"
             "float, center, class:(Xdg-desktop-portal-[a-zA-z0-9]*)"
           ]
