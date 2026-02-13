9621ea79a4630877cfcb9321e3f282cd46901971
Cedric Lehr <info@ceedri.ch>
2026-02-13 10:22:35 +0100

    satine: 164	2026-02-13	10:22:33	25.11.20260117.72ac591	6.12.65	Unknown	[]

--- nixos/nixosModules/hyprland/default.nix
+++ nixos/nixosModules/hyprland/default.nix
@@ -172,8 +172,10 @@ in {
 
         # Animation
         animation = [
+          "windows, 1, 0.5, easeOutQuint"
+          "windowsIn, 1, 0.5, easeOutQuint, popin 80%"
+          "windowsOut, 1, 0.5, linear, popin 80%"
           "workspaces, 1, 2, default"
-          "windows, 1, 0.5, easeOutQuint, popin 80%"
           "fade, 1, 0.5, default"
         ];
 
