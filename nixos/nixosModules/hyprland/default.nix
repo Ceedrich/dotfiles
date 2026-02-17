a0156f0122b9056769df34b7c0bdc720718ec392
Cedric Lehr <info@ceedri.ch>
2026-02-17 09:39:07 +0100

    ahsoka: 490	2026-02-17	09:39:01	25.11.20260214.3aadb7c	6.12.70	Unknown	[]

--- nixos/nixosModules/hyprland/default.nix
+++ nixos/nixosModules/hyprland/default.nix
@@ -242,7 +242,11 @@ in {
     xdg.portal = {
       enable = true;
       wlr.enable = true;
-      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-gnome
+      ];
       configPackages = [pkgs.xdg-desktop-portal-hyprland];
     };
 
