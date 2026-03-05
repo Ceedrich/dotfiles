f5fda4e38e3dfb24d18b98113d35d23f96882492
Cedric Lehr <info@ceedri.ch>
2026-03-05 17:13:29 +0100

    ahsoka: 523	2026-03-05	17:13:27	25.11.20260225.1267bb4	6.12.74	Unknown	[]

--- nixos/nixosModules/firefox/default.nix
+++ nixos/nixosModules/firefox/default.nix
@@ -19,7 +19,7 @@ in
           "cookiebanners.service.mode" = 2; # Block cookie banners
           "privacy.donottrackheader.enabled" = true;
           "privacy.fingerprintingProtection" = true;
-          "privacy.resistFingerprinting" = true;
+          "privacy.resistFingerprinting" = false;
           "privacy.trackingprotection.emailtracking.enabled" = true;
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.fingerprinting.enabled" = true;
