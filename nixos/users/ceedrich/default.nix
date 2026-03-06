65d9c4f3a79889f33837bda8a77248adadba4352
Cedric Lehr <info@ceedri.ch>
2026-03-06 19:47:55 +0100

    ahsoka: 540	2026-03-06	19:47:53	25.11.20260225.1267bb4	6.12.74	Unknown	[]

--- nixos/users/ceedrich/default.nix
+++ nixos/users/ceedrich/default.nix
@@ -2,7 +2,7 @@
   programs.zsh.enable = true;
   users.users.ceedrich = {
     initialPassword = "password";
-    shell = pkgs.zsh;
+    shell = pkgs.bash;
     openssh.authorizedKeys.keys = [
       # Laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICuBEOPD9FWQSesLFFfNEDlU7xoT8fEL+EGJJIluzSmO ceedrich@satine"
