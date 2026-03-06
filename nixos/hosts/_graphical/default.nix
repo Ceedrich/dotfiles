73dd030c4f4f5645f71ef3327f5308f3a140472f
Cedric Lehr <info@ceedri.ch>
2026-03-06 10:12:56 +0100

    satine: 229	2026-03-06	09:38:38	25.11.20260225.1267bb4	6.12.74	Unknown	[]

--- nixos/hosts/_graphical/default.nix
+++ nixos/hosts/_graphical/default.nix
@@ -18,10 +18,10 @@ in {
       programs.nwg-drawer.enable = true;
       xdg.mimeApps.defaultApplications = {
         "text/html" = "brave-browser.desktop";
-        "x-scheme-handler/http" = "brave-browser.desktop";
-        "x-scheme-handler/https" = "brave-browser.desktop";
-        "x-scheme-handler/about" = "brave-browser.desktop";
-        "x-scheme-handler/unknown" = "brave-browser.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
       };
       programs.waybar = {
         enable = mkDefault true;
