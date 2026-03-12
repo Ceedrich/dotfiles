ffbd8b9b5eb74c470745a6d8111ce31466500b1c
Cedric Lehr <info@ceedri.ch>
2026-03-12 17:25:29 +0100

    ahsoka: 588	2026-03-12	17:25:27	25.11.20260225.1267bb4	6.12.74	Unknown	[]

--- nixos/nixosModules/user-mime.nix
+++ nixos/nixosModules/user-mime.nix
@@ -1,5 +1,28 @@
-{config, ...}: {
-  config.global-hm.config = {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  global-hm.config = {
     xdg.mimeApps.defaultApplications = config.xdg.mime.defaultApplications;
+  };
+
+  system.userActivationScripts = {
+    xdg-setup = let
+      handlr = "${lib.getExe pkgs.handlr-regex}";
+      makeLine = mime: app: "${handlr} set ${mime} ${app}";
+    in {
+      text = lib.pipe config.xdg.mime.defaultApplications [
+        (lib.mapAttrsToList (
+          mime: apps:
+            if lib.isString apps
+            then [(makeLine mime apps)]
+            else lib.map (makeLine mime) apps
+        ))
+        lib.lists.flatten
+        (lib.concatStringsSep "\n")
+      ];
+    };
   };
 }
