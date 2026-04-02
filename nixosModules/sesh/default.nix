5bd0467057327de2e5d21812f76c25ec1e82b970
Cedric Lehr <info@ceedri.ch>
2026-04-02 15:03:39 +0200

    Update host/satine from main

--- nixosModules/sesh/default.nix
+++ nixosModules/sesh/default.nix
@@ -12,40 +12,12 @@ in {
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [cfg.package];
-    programs.zsh.interactiveShellInit =
-      #sh
-      ''
-        function sesh-sessions() {
-          {
-            sesh connect $(sesh list | fzf)
-          }
-        }
-
-        zle     -N             sesh-sessions
-        bindkey -M emacs '^T' sesh-sessions
-        bindkey -M vicmd '^T' sesh-sessions
-        bindkey -M viins '^T' sesh-sessions
-      '';
-    programs.tmux.extraConfig =
-      ''
-        bind-key x kill-pane # skip "kill-pane 1? (y/n)" prompt
-        set -g detach-on-destroy off  # don't exit from tmux when closing a session
-      ''
-      + ''
-        bind-key "T" run-shell "sesh connect \"$(
-          sesh list --icons | fzf --tmux -p 80%,70% \
-            --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-            --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
-            --bind 'tab:down,btab:up' \
-            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
-            --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
-            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
-            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
-            --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
-            --preview-window 'right:55%' \
-            --preview 'sesh preview {}'
-        )\""
-      '';
+    home-manager.sharedModules = [
+      ({...}: {
+        programs.sesh = {
+          enable = true;
+        };
+      })
+    ];
   };
 }
