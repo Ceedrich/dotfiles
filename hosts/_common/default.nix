349ff9c897aac407b9ddd1c64a6e80f3bd07a242
Cedric Lehr <info@ceedri.ch>
2026-04-03 11:26:50 +0200

    update bootloader config

--- hosts/_common/default.nix
+++ hosts/_common/default.nix
@@ -53,7 +53,11 @@
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
