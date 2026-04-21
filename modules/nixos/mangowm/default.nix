209ef3225a8cd4cdeb337fbc37fcb0b8b6d70864
Cedric Lehr <info@ceedri.ch>
2026-04-21 11:37:54 +0200

    add mango hm config

--- modules/nixos/mangowm/default.nix
+++ modules/nixos/mangowm/default.nix
@@ -2,5 +2,18 @@
   flake.nixosModules.mangowm = {
     imports = [inputs.mangowm.nixosModules.mango];
     programs.mango.enable = true;
+
+    home-manager.sharedModules = [
+      inputs.mangowm.hmModules.mango
+      {
+        wayland.windowManager.mango = {
+          enable = true;
+          systemd = {
+            enable = true;
+            xdgAutostart = true;
+          };
+        };
+      }
+    ];
   };
 }
