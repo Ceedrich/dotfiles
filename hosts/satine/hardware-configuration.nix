f39e6656dc5ccc437c0e92a4c0bcd87273f0cc95
Cedric Lehr <info@ceedri.ch>
2026-04-26 19:29:57 +0200

    fix hibernation

--- hosts/satine/hardware-configuration.nix
+++ hosts/satine/hardware-configuration.nix
@@ -38,7 +38,7 @@
     }
   ];
 
-  boot.resumeDevice = "/var/lib/swapfile";
+  boot.initrd.systemd.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
