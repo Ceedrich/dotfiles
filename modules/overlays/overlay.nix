403b693d109104af73aeb50b5357a3271bc3fb80
Cedric Lehr <info@ceedri.ch>
2026-04-27 23:21:40 +0200

    update rofi overlay

--- modules/overlays/overlay.nix
+++ modules/overlays/overlay.nix
@@ -9,11 +9,12 @@
     ceedrichLib = self.lib;
     ceedrichVim = inputs.ceedrichVim.packages.${system}.neovim;
     tailscale = inputs.nixpkgs-unstable.legacyPackages.${system}.tailscale;
-    rofi = prev.rofi.overrideAttrs (old: {
+    rofi-unwrapped = prev.rofi-unwrapped.overrideAttrs (old: {
       postInstall =
         old.postInstall or ""
         + ''
           ln -s $out/bin/rofi $out/bin/dmenu
+          rm -rf $out/share/applications/rofi-theme-selector.desktop
         '';
     });
     hyprshutdown = inputs.nixpkgs-unstable.legacyPackages.${system}.hyprshutdown;
