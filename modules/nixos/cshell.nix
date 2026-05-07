7ef2a60390994ac89d64a4d903729e09dd8604a7
Cedric Lehr <info@ceedri.ch>
2026-05-07 16:17:36 +0200

    Update host/satine from testing 418	2026-05-07	16:17:59	25.11.20260501.26ef669	6.12.85	Unknown	[]

--- modules/nixos/cshell.nix
+++ modules/nixos/cshell.nix
@@ -1,5 +1,22 @@
 {inputs, ...}: {
-  flake.nixosModules.cshell = {pkgs, ...}: {
-    environment.systemPackages = [inputs.cshell.packages.${pkgs.stdenv.hostPlatform.system}.default];
+  flake.nixosModules.cshell = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    package = inputs.cshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  in {
+    environment.systemPackages = [package];
+
+    systemd.user.services."cshell" = {
+      enable = true;
+      wantedBy = ["graphical-session.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        description = "Cshell, MY graphical shell";
+        ExecStart = lib.getExe package;
+      };
+    };
   };
 }
