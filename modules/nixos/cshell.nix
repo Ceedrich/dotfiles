e7251f48661fa9dad4d59bd5abe5f6e7e3820105
Cedric Lehr <info@ceedri.ch>
2026-05-07 16:16:40 +0200

    add cshell service config

--- modules/nixos/cshell.nix
+++ modules/nixos/cshell.nix
@@ -1,5 +1,22 @@
 {inputs, ...}: {
-  flake.nixosModules.cshell = {pkgs, ...}: {
-    environment.systemPackages = [inputs.cshell.packages.${pkgs.stdenv.hostPlatform.system}.default];
+  flake.nixosModules.cshell = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    package = inputs.cshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  in {
+    environment.systemPackages = [package];
+
+    systemd.user.services."cshell" = {
+      enable = true;
+      wantedBy = ["graphical-session.target"];
+      description = "Cshell, MY graphical shell";
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = lib.getExe package;
+      };
+    };
   };
 }
