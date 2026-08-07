6bd5782bec75862f85b50110507be85c75297215
Cedric Lehr <info@ceedri.ch>
2026-08-07 23:05:08 +0200

    Update host/satine from hyprland-lua 541	2026-08-07	23:05:33	26.05.20260719.fd14620	6.18.39	Unknown	[]

---
+++ modules/nixos/hyprland/_hypr/plugins/hyprbars.lua
@@ -0,0 +1,26 @@
+hl.config({
+	plugin = {
+		hyprbars = {
+			bar_height = 28,
+			bar_button_padding = 8,
+			bar_blur = true,
+			bar_color = "$crust",
+			col = { text = "$overlay0" },
+			bar_text_size = 14,
+			bar_text_font = "JetBrains Mono Nerdfont",
+			bar_part_of_window = true,
+			bar_precedence_over_border = true,
+			bar_buttons_alignment = "left",
+		},
+	},
+})
+
+local function addBtn(bg_color, action)
+	hl.plugin.hyprbars.add_button({
+		size = 14,
+		bg_color,
+		action,
+	})
+end
+
+addBtn("$red", hl.notification.create({ text = "hello there", timeout = 5000 }))
