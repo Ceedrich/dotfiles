b2bcf0764fdff8c034a6435def329313df19f72d
Cedric Lehr <info@ceedri.ch>
2026-08-07 22:45:43 +0200

    Update host/satine from hyprland-lua 539	2026-08-07	22:46:11	26.05.20260719.fd14620	6.18.39	Unknown	[]

--- modules/nixos/hyprland/_hypr/animations.lua
+++ modules/nixos/hyprland/_hypr/animations.lua
@@ -1,3 +1,3 @@
-hl.animation({ leaf = "workspaces", enabled = true, speed = 2, spring = "default" })
-hl.animation({ leaf = "windows", enabled = true, speed = 1, spring = "default", style = "popin 90%" })
-hl.animation({ leaf = "fade", enabled = true, speed = 0.5, spring = "default" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 2, bezier = "default" })
+hl.animation({ leaf = "windows", enabled = true, speed = 1, bezier = "default", style = "popin 90%" })
+hl.animation({ leaf = "fade", enabled = true, speed = 0.5, bezier = "default" })
