ae7c1c20aa99f4d709b77d5e5c92d360f0457cbc
Cedric Lehr <info@ceedri.ch>
2026-08-07 23:15:09 +0200

    Update host/satine from hyprland-lua 545	2026-08-07	23:15:38	26.05.20260719.fd14620	6.18.39	Unknown	[]

--- modules/nixos/hyprland/_hypr/plugins/hyprbars.lua
+++ modules/nixos/hyprland/_hypr/plugins/hyprbars.lua
@@ -1,28 +1,30 @@
+local colors = require("themes.catppuccin")
+
 if hl.plugin.hyprbars ~= nil then
-  hl.config({
-    plugin = {
-      hyprbars = {
-        bar_height = 28,
-        bar_button_padding = 8,
-        bar_blur = true,
-        bar_color = "$crust",
-        col = { text = "$overlay0" },
-        bar_text_size = 14,
-        bar_text_font = "JetBrains Mono Nerdfont",
-        bar_part_of_window = true,
-        bar_precedence_over_border = true,
-        bar_buttons_alignment = "left",
-      },
-    },
-  })
+	hl.config({
+		plugin = {
+			hyprbars = {
+				bar_height = 28,
+				bar_button_padding = 8,
+				bar_blur = true,
+				bar_color = "$crust",
+				col = { text = "$overlay0" },
+				bar_text_size = 14,
+				bar_text_font = "JetBrains Mono Nerdfont",
+				bar_part_of_window = true,
+				bar_precedence_over_border = true,
+				bar_buttons_alignment = "left",
+			},
+		},
+	})
 
-  local function addBtn(bg_color, action)
-    hl.plugin.hyprbars.add_button({
-      size = 14,
-      bg_color,
-      action,
-    })
-  end
+	local function addBtn(bg_color, action)
+		hl.plugin.hyprbars.add_button({
+			size = 14,
+			bg_color,
+			action,
+		})
+	end
 
-  addBtn("$red", hl.notification.create({ text = "hello there", timeout = 5000 }))
+	addBtn(colors.red, hl.notification.create({ text = "hello there", timeout = 5000 }))
 end
