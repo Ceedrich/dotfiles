874040522a21a5437cb9530d31bab5ea23efebf7
Cedric Lehr <info@ceedri.ch>
2026-08-09 10:39:59 +0200

    Update host/satine from hyprland-lua 558	2026-08-09	10:40:27	26.05.20260807.ee48b14	6.18.43	Unknown	[]

--- modules/nixos/hyprland/_hypr/plugins/scrolloverview.lua
+++ modules/nixos/hyprland/_hypr/plugins/scrolloverview.lua
@@ -1,22 +1,22 @@
 if hl.plugin.scrolloverview ~= nil then
-  hl.config({
-    plugin = {
-      scrolloverview = {
-        gesture_distance = 300, -- how far is the "max" for the gesture
-        scale = 0.8,        -- preferred overview scale
-        workspace_gap = 30,
-        layout = "horizontal", -- vertical or horizontal
-        wallpaper = 0,      -- 0: global only, 1: per-workspace only, 2: both
-        blur = true,        -- blur only the main overview wallpaper
+	hl.config({
+		plugin = {
+			scrolloverview = {
+				gesture_distance = 20, -- how far is the "max" for the gesture
+				scale = 0.8, -- preferred overview scale
+				workspace_gap = 30,
+				layout = "horizontal", -- vertical or horizontal
+				wallpaper = 0, -- 0: global only, 1: per-workspace only, 2: both
+				blur = false, -- blur only the main overview wallpaper
 
-        shadow = {
-          enabled = false,
-        },
-      },
-    },
-  })
+				shadow = {
+					enabled = false,
+				},
+			},
+		},
+	})
 
-  hl.bind("SUPER + g", function()
-    hl.plugin.scrolloverview.overview("toggle all")
-  end)
+	hl.bind("SUPER + g", function()
+		hl.plugin.scrolloverview.overview("toggle all")
+	end)
 end
